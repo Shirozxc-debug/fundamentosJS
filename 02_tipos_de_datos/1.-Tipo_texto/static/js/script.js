6ea357af37d746b49bb798c7fff41c69fc291df3
Shirozxc-debug <matiassantelices@liceovvh.cl>
2026-04-15 08:10:07 -0400

    f

---
+++ 02_tipos_de_datos/1.-Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("Conexión con js correcta...");
+/*
+Datos: Tipo texto(String)
+Concatentación de texto con el signo +
+Podemos unir texto y variables
+*/
+const nombre = "Matias";
+const apellido = "Santelices";
+//Unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
